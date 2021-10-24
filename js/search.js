8ee389bd61f4ad8a50576e823724558765ecfec7
nitigya gurung <grgniti07@gmail.com>
2021-10-24 21:19:40 +0100

    Search form validation

--- js/search.js
+++ js/search.js
@@ -1,16 +1,16 @@
 const queryString = window.location.search;
 console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
-
 const PIN = urlParams.get('PIN');
-
-
 const dis = urlParams.get('dis')
-
-
-document.getElementById("demo1").innerHTML= "Your Pin Code is:"   +PIN ;
-document.getElementById("demo2").innerHTML= "Your District Code is:"   +dis ;
+document.getElementById("pin").innerHTML= "Your Pin Code is:"   +PIN ;
+document.getElementById("district").innerHTML= "Your District Code is:"   +dis ;
  
- 
+//Spinner
+function spinner() {
+    const load ='<div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>';
+    document.getElementById(searchbtn);
+}
+
 
   
